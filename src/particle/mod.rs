274daf5da5ad33dec3181ba2bedb11641ec6d168
Jonathan Reem <jonathan.reem@gmail.com>
2014-06-08 20:28:38 -0700

    (feat) Added sample particle type.

--- src/particle/mod.rs
+++ src/particle/mod.rs
@@ -13,3 +13,22 @@ pub trait Particle {
     self.pos().mut_add(&vel);
   }
 }
+
+pub struct PlainParticle {
+  pub pos: Vector,
+  pub vel: Vector,
+  pub acc: Vector
+}
+
+impl Particle for PlainParticle {
+  #[inline]
+  fn pos<'a>(&'a mut self) -> &'a mut Vector { return &mut self.pos; }
+  #[inline]
+  fn vel<'a>(&'a mut self) -> &'a mut Vector { return &mut self.vel; }
+  #[inline]
+  fn acc<'a>(&'a mut self) -> &'a mut Vector { return &mut self.acc; }
+  #[inline]
+  fn from_vectors(p: Vector, v: Vector, a: Vector) -> PlainParticle{
+    return PlainParticle { pos: p, vel: v, acc: a }
+  }
+}
