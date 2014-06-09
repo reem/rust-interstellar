fcb946a88b14c1ba02dac666e727fa7c6ecc7cd4
Jonathan Reem <jonathan.reem@gmail.com>
2014-06-08 20:24:00 -0700

    (feat) Added exports.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,16 @@
-#![crate_id = ""]
-#![deny(missing_doc)]
+#![crate_id = "interstellar"]
+//#![deny(missing_doc)]
+#![deny(unused_result)]
+#![deny(deprecated_owned_vector)]
+#![deny(unnecessary_qualification)]
+#![feature(globs)]
 
-//! Documentation goes here.
+//! A fast 2d particle physics library for simple and pretty
+//! simulations.
 
+pub mod draw;
+pub mod field;
+pub mod emitter;
+pub mod particle;
+pub mod vector;
+pub mod state;
