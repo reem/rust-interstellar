fc6fef0d84c32e42b5aeed3dcf4dbdc90e4334c0
Jonathan Reem <jonathan.reem@gmail.com>
2014-06-08 20:29:22 -0700

    (feat) Added sample field type as Gravity.

--- src/field/mod.rs
+++ src/field/mod.rs
@@ -5,3 +5,18 @@ use super::draw::{Drawable, Color};
 pub trait Field {
   fn affect<P: Particle>(&self, &mut P) -> ();
 }
+
+pub struct Gravity {
+  pub pos:  Vector,
+  pub mass: f64 
+}
+
+impl Field for Gravity {
+  fn affect<P: Particle>(&self, particle: &mut P) -> () {
+    let xDiff = self.pos.x - particle.pos().x;
+    let yDiff = self.pos.x - particle.pos().y;
+    let force = self.mass / (xDiff.powf(2f64) + yDiff.powf(2f64)).powf(1.5);
+
+    particle.acc().fastAdd(xDiff * force, yDiff * force);
+  }
+}
