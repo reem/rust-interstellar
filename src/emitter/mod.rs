162dd39021628d654e8848945fa6ce7f8e49510e
Jonathan Reem <jonathan.reem@gmail.com>
2014-06-08 20:29:57 -0700

    (feat) Added sample StreamEmitter

--- src/emitter/mod.rs
+++ src/emitter/mod.rs
@@ -8,3 +8,26 @@ pub trait Emitter<P: Particle> {
   fn emit(&mut self) -> Vec<P>;
 }
 
+pub struct StreamEmitter {
+  pos: Vector,
+  direction: f64,
+  ppf: uint,
+  startVel: f64,
+  spread: f64
+}
+
+impl Emitter<PlainParticle> for StreamEmitter {
+  fn emit(&mut self) -> Vec<PlainParticle> {
+    range(0, self.ppf).map(|_| {
+      Particle::from_vectors(
+        self.pos,
+        Vector::from_angle(
+          self.startVel * random::<f64>(), 
+          self.spread - (random::<f64>() * self.spread * 2f64) + self.direction
+        ),
+        Vector { x: 0f64, y: 0f64}
+      )
+    }).collect()
+  }
+}
+
