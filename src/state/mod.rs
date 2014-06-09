b71a3cf12f86c90fe1a2eb70caaed96d99d5e4e1
Jonathan Reem <jonathan.reem@gmail.com>
2014-06-08 22:15:58 -0700

    (feat) Added State impl, including step.

--- src/state/mod.rs
+++ src/state/mod.rs
@@ -11,3 +11,28 @@ pub struct State <F, P, E> {
   max_pos: Vector,
   min_pos: Vector
 }
+
+impl<F: Field, P: Particle, E: Emitter<P>> State<F, P, E> {
+  fn step(&mut self) {
+    if self.particles.len() < self.particle_limit {
+      for emitter in self.emitters.mut_iter() {
+        self.particles.extend(emitter.emit().move_iter());
+      }
+    }
+
+    for particle in self.particles.mut_iter() {
+      particle.move();
+    }
+
+    for particle in self.particles.mut_iter() {
+      *particle.acc_mut() = Vector { x: 0f64, y: 0f64 };
+      for field in self.fields.iter() {
+        field.affect(particle);
+      }
+    }
+
+    let min = self.min_pos;
+    let max = self.max_pos;
+    self.particles.retain(|ref p| { p.pos() < max && p.pos() > min });
+  }
+}
