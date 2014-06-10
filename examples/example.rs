7e28579fa71d1d28537f9f38b75119fdf82b4024
Jonathan Reem <jonathan.reem@gmail.com>
2014-06-10 09:56:15 -0700

    (cleanup) Tweaked defaults for example to work around bug in field positioning.

--- examples/example.rs
+++ examples/example.rs
@@ -26,11 +26,11 @@ fn main() {
       StreamEmitter { 
         pos: Vector { x: 100.0, y: 400.0 },
         direction: 0.0,
-        ppf: 10,
-        startVel: 5.0,
+        ppf: 5,
+        startVel: 4.0,
         spread: 0.2 }
     ),
-    fields: vec!(Gravity { pos: Vector { x: 500.0, y: 400.0 }, mass: -500.0 }),
+    fields: vec!(Gravity { pos: Vector { x: 400.0, y: 400.0 }, mass: -1000.0 }),
     particles: particles,
     particle_limit: 100000,
     max_pos: Vector { x: 1400.0, y: 900.0 },
