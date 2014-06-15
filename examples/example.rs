b47aec32464bbf2bfce8c09eb5c1062788885b3d
Jonathan Reem <jonathan.reem@gmail.com>
2014-06-14 20:06:42 -0700

    (refactor) Refactored example to use new Vector.

--- examples/example.rs
+++ examples/example.rs
@@ -19,22 +19,22 @@ fn main() {
       exit_on_esc: true,
       background_color: [0.0, 0.0, 0.0, 0.0],
     }
-  ); 
+  );
   let particles: Vec<PlainParticle> = vec!();
   let mut simulation = State {
     emitters: vec!(
-      StreamEmitter { 
-        pos: Vector { x: 100.0, y: 400.0 },
+      StreamEmitter {
+        pos: Vector::new(100.0, 400.0),
         direction: 0.0,
         ppf: 5,
         startVel: 4.0,
         spread: 0.2 }
     ),
-    fields: vec!(Gravity { pos: Vector { x: 400.0, y: 400.0 }, mass: -1000.0 }),
+    fields: vec!(Gravity { pos: Vector::new(400.0, 400.0), mass: -1000.0 }),
     particles: particles,
     particle_limit: 100000,
-    max_pos: Vector { x: 1400.0, y: 900.0 },
-    min_pos: Vector { x: 0.0, y: 0.0 }
+    max_pos: Vector::new(1400.0, 900.0),
+    min_pos: Vector::new(0.0, 0.0)
   };
   simulation.run(&mut window, &mut AssetStore::empty());
 }
